c21f8b36198a2120f05f7b57a6f33d3e4b0d407d
Administrator <ebenezarsileshi@gmail.com>
2024-12-13 12:23:24 +0300

    add api to create books

--- BookApp/api.py
+++ BookApp/api.py
@@ -1,22 +1,34 @@
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-
+from .models import BookModel
 @api_view(['GET'])
 @renderer_classes([JSONRenderer])  # Force JSON response
 def BookListApi(request):
+    # fetch from the database
+
+    books = BookModel.objects.all()
+
     books = [
         {
-            'name': 'The Alchemist',
-            'author': 'Paulo Coelho',
-        },
-        {
-            'name': 'The Monk who Sold',
-            'author': 'Robin Sharma',
-        },
-        {
-            'name': 'The 5 AM Club',
-            'author': 'Robin Sharma',
-        }
-    ]
+            'name': book.name ,
+            'author': book.author
+        } for book in books]
+
+    # send response
     return Response(books)
+
+
+@api_view(['POST'])
+def BookCreateApi(request):
+
+    data = request.data
+
+    name = data.name
+    author = data.author
+
+    BookModel(name = name, author = author).save()
+
+    return Response({
+        'message': 'Book Created'
+    })
--- BookApp/urls.py
+++ BookApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .api import BookListApi  # Ensure this import is valid
+from .api import BookListApi, BookCreateApi  # Ensure this import is valid
 
 urlpatterns = [
     path('list/', BookListApi, name='book-list'),  # Ensure the trailing slash
+    path('create', BookCreateApi , name = 'book-create'),
 ]
